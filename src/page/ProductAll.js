c66f02640b61e6d35d017bf741ca8499996b3fcf
MiHyeon <kkmmhh7478@gmail.com>
2024-08-22 13:53:27 +0900

    #5 responsible design

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -15,7 +15,7 @@ const ProductAll = () => {
 
   return (
     <div>
-      <div className="container mx-auto grid grid-cols-4">
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center">
         {productList.map((item, index) => (
           <ProductCard item={item} key={index}/>
         ))}
