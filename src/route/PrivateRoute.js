c516466e6a93bd4654939f6bb5069623d2f59157
MiHyeon <kkmmhh7478@gmail.com>
2024-08-23 11:53:53 +0900

    privateRoute 경로 오류 수정

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({authenticate, type}) => {
   return (
-    authenticate? <ProductDetail />:<New />
+    authenticate == true ? <ProductDetail /> : <Navigate to="/shopping-project/login" />
   )
 }
 
