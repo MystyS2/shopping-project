54a91356d517520367c805b6e2f481205f6d6b9c
MiHyeon <kkmmhh7478@gmail.com>
2024-08-23 21:25:45 +0900

    #13

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { NextUIProvider } from "@nextui-org/react";
 import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import ProductAll from './page/ProductAll';
-import ProductDetail from './page/ProductDetail';
 import Login from './page/Login';
 import Navbar from './component/Navbar';
 import { useEffect, useState } from 'react';
 import PrivateRoute from './route/PrivateRoute';
-import New from './page/New';
+import HotNew from './page/HotNew';
+import ProductList from './page/ProductList';
 
 // 0. 메인 컬러 : #9face1 하이라이트 컬러 : #ffd05c 그림자 컬러 : #5168c5 배경 컬러 : #faf4dc
 // 1. 전체 상품 페이지, 로그인, 상품 상세페이지
@@ -31,8 +31,13 @@ function App() {
           <Route path='/shopping-project/login' element={<Login setAuthenticate={setAuthenticate} />} />
           <Route path='/shopping-project/product' element={<ProductAll />} />
           <Route path='/shopping-project/product/:id' element={<PrivateRoute authenticate={authenticate} type='all' />} />
-          <Route path='/shopping-project/hot' element={<ProductAll />} />
-          <Route path='/shopping-project/new' element={<New />} />
+          <Route path='/shopping-project/hot' element={<HotNew category="hot" />} />
+          <Route path='/shopping-project/new' element={<HotNew category="new" />} />
+          <Route path='/shopping-project/long' element={<ProductList category="long" />} />
+          <Route path='/shopping-project/short' element={<ProductList category="short" />} />
+          <Route path='/shopping-project/gel' element={<ProductList category="gel" />} />
+          <Route path='/shopping-project/polish' element={<ProductList category="polish" />} />
+          <Route path='/shopping-project/clothes' element={<ProductList category="clothes" />} />
         </Routes>
       </div>      
     </NextUIProvider>
---
+++ src/page/ProductList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../component/ProductCard';
+
+const ProductList = ({category}) => {
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products/`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <div className="product-all container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center">
+        {productList.filter((key)=>key.category===`${category}`).map((item, index) => (<ProductCard item={item} key={index} />))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
